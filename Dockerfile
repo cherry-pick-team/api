63143d47197ce1e2619fc41da2c257d0ae447db4
ValerGit <v.pershenkova@yahoo.com>
2017-04-08 21:24:49 +0300

    Fix

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM python:3.5.2-slim
+
+WORKDIR /usr/src/app
+ENV PYTHONPATH=/usr/src/app
+
+COPY ./requirements.txt /usr/src/app/
+
+RUN apt-get update \
+     && apt-get install -y libpq-dev gcc \
+     && pip install --no-cache-dir -r requirements.txt 
+
+COPY . /usr/src/app
+
+CMD ["python", "cherry-pick-api.py"]
--- config.py
+++ config.py
@@ -1,10 +1,16 @@
 import os
+import sys
 
 from external import data_getter
 from external import genius
 
 
-sphinx = data_getter.SphinxSearch('localhost', 9306, '', '')
-postgres = data_getter.PsgClient('postgres', os.environ['PSQL_PASSWORD'], 'track_bar')
-cropper = data_getter.CropperDemon('localhost', 8080)
-genius = genius.Genius(os.environ['GENIUS_KEY'])
+try:
+    sphinx = data_getter.SphinxSearch(os.environ['SPHINX_HOST'], 9306, '', '')
+    postgres = data_getter.PsgClient(os.environ['PSQL_HOST'], os.environ['PSQL_PASSWORD'], 'track_bar')
+    cropper = data_getter.CropperDemon(os.environ['CROPPER_HOST'], 8080)
+    genius = genius.Genius(os.environ['GENIUS_KEY'])
+except Exception as e:
+    print(e)
+    sys.exit(1)
+
