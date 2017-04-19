7b03fef9ff7136ffd0ffb70b473f2c081a165e05
Nikita <nks.off@gmail.com>
2017-04-19 23:49:40 +0300

    fix cropper port

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ from external import genius
 try:
     sphinx = data_getter.SphinxSearch(os.environ['SPHINX_HOST'], 9306, '', '')
     postgres = data_getter.PsgClient(os.environ['PSQL_HOST'], os.environ['PSQL_USER'], os.environ['PSQL_PASSWORD'], 'track_bar')
-    cropper = data_getter.CropperDemon(os.environ['CROPPER_HOST'], 8080)
+    cropper = data_getter.CropperDemon(os.environ['CROPPER_HOST'], 8880)
     genius = genius.Genius(os.environ['GENIUS_KEY'])
 except Exception as e:
     print(e)
